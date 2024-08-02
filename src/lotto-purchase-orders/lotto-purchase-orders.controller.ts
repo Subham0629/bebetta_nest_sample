a17c6daebe933995a4cb1bde0b77906df202342a
Subham0629 <kandarisubham72@gmail.com>
2024-08-02 18:45:13 +0530

    Added additional APIs

--- src/lotto-purchase-orders/lotto-purchase-orders.controller.ts
+++ src/lotto-purchase-orders/lotto-purchase-orders.controller.ts
@@ -52,6 +52,46 @@ export class LottoPurchaseOrdersController {
 
     return this.lottoPurchaseOrdersService.updateByIdOrPassNumber(id, pass_number, updateLottoPurchaseOrderDto);
   }
+
+  @Get('users-by-variant/:variant_id')
+  async getUsersOrderCountByVariantId(
+    @Param('variant_id') variant_id: string,
+    @Query('page') page: string,
+    @Query('limit') limit: string
+  ) {
+    const variantId = parseInt(variant_id, 10);
+    const pageNum = parseInt(page, 10) || 1;
+    const limitNum = parseInt(limit, 10) || 10;
+
+return this.lottoPurchaseOrdersService.getUsersOrderCountByVariantId(variantId, pageNum, limitNum);
+  }
+  
+
+  @Get('get-pass/:user_id/:variant_id')
+  async findPassNumbersByUserAndVariant(
+    @Param('user_id') user_id: string,
+    @Param('variant_id') variant_id: string,
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ) {
+    return this.lottoPurchaseOrdersService.findPassNumbersByUserAndVariant(
+      parseInt(user_id, 10),
+      parseInt(variant_id, 10),
+      parseInt(page, 10),
+      parseInt(limit, 10),
+    );
+  }
+
+  @Get('get-winner/:variant_id/:win_rank')
+  async findOrdersByVariantAndWinRank(
+    @Param('variant_id') variant_id: string,
+    @Param('win_rank') win_rank: string
+  ) {
+    return this.lottoPurchaseOrdersService.findOrdersByVariantAndWinRank(
+      parseInt(variant_id, 10),
+      parseInt(win_rank, 10)
+    );
+  }
 }
 
 
--- src/lotto-purchase-orders/lotto-purchase-orders.service.ts
+++ src/lotto-purchase-orders/lotto-purchase-orders.service.ts
@@ -7,8 +7,11 @@ import { UpdateLottoPurchaseOrderDto } from './dto/update-lotto-purchase-order.d
 export class LottoPurchaseOrdersService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createLottoPurchaseOrderDto: CreateLottoPurchaseOrderDto) {
+  generateEightDigitNumber(): number {
+    return Math.floor(10000000 + Math.random() * 90000000);
+  }
 
+  async create(createLottoPurchaseOrderDto: CreateLottoPurchaseOrderDto) {
     const variant = await this.prisma.tbl_lotto_variants.findUnique({
       where: { id: createLottoPurchaseOrderDto.variant_id },
     });
@@ -17,29 +20,47 @@ export class LottoPurchaseOrdersService {
       throw new BadRequestException('Invalid variant_id.');
     }
 
-    // Check if the purchase currency is coin
+    // Checking if the purchase currency is coin
     if (createLottoPurchaseOrderDto.purchase_currency === 'coin') {
-      // Count the existing orders for the given variant_id
-      const existingOrdersCount = await this.prisma.tbl_lotto_purchase_orders.count({
-        where: { variant_id: createLottoPurchaseOrderDto.variant_id,
-          user_id: createLottoPurchaseOrderDto.user_id },
-      });
-      
-      // Compare the count with bet_coin_purchase_limit
+      // Counting the existing orders for the given variant_id
+      const existingOrdersCount =
+        await this.prisma.tbl_lotto_purchase_orders.count({
+          where: {
+            variant_id: createLottoPurchaseOrderDto.variant_id,
+            user_id: createLottoPurchaseOrderDto.user_id,
+          },
+        });
+
+      // Comparing the count with bet_coin_purchase_limit
       if (existingOrdersCount >= variant.bet_coin_purchase_limit) {
-        throw new BadRequestException('Purchase limit exceeded for this LOTTO.');
+        throw new BadRequestException(
+          'Purchase limit exceeded for this LOTTO.',
+        );
       }
     }
+    // Generating an 8-digit random number for pass_number
+    let passNumber = this.generateEightDigitNumber();
 
-    const existingPassNumber = await this.prisma.tbl_lotto_purchase_orders.findFirst({
-      where: {
-        pass_number: createLottoPurchaseOrderDto.pass_number,
-      },
-    });
+    // Checking if the generated pass_number already exists
+    let existingPassNumber =
+      await this.prisma.tbl_lotto_purchase_orders.findFirst({
+        where: {
+          pass_number: passNumber,
+        },
+      });
 
-    if (existingPassNumber) {
-      throw new BadRequestException('Pass number already exists for this LOTTO');
+    // If the pass_number already exists, generating a new one until a unique one is found
+    while (existingPassNumber) {
+      passNumber = this.generateEightDigitNumber();
+      existingPassNumber =
+        await this.prisma.tbl_lotto_purchase_orders.findFirst({
+          where: {
+            pass_number: passNumber,
+          },
+        });
     }
+
+    createLottoPurchaseOrderDto.pass_number = passNumber;
     return this.prisma.tbl_lotto_purchase_orders.create({
       data: createLottoPurchaseOrderDto,
     });
@@ -70,7 +91,11 @@ export class LottoPurchaseOrdersService {
     });
   }
 
-  async updateByIdOrPassNumber(id: number | null, pass_number: number | null, updateLottoPurchaseOrderDto: UpdateLottoPurchaseOrderDto) {
+  async updateByIdOrPassNumber(
+    id: number | null,
+    pass_number: number | null,
+    updateLottoPurchaseOrderDto: UpdateLottoPurchaseOrderDto,
+  ) {
     if (id) {
       return this.prisma.tbl_lotto_purchase_orders.update({
         where: { id },
@@ -82,9 +107,131 @@ export class LottoPurchaseOrdersService {
         data: updateLottoPurchaseOrderDto,
       });
     } else {
-      throw new BadRequestException('Either id or pass_number must be provided.');
+      throw new BadRequestException(
+        'Either id or pass_number must be provided.',
+      );
     }
   }
-  
 
+  async getUsersOrderCountByVariantId(
+    variant_id: number,
+    page: number = 1,
+    limit: number = 10,
+  ) {
+    const skip = (page - 1) * limit;
+
+    const usersOrderCount = await this.prisma.tbl_lotto_purchase_orders.groupBy(
+      {
+        by: ['user_id'],
+        where: { variant_id },
+        _count: {
+          user_id: true,
+        },
+        skip,
+        take: limit,
+        orderBy: {
+          user_id: 'asc',
+        },
+      },
+    );
+
+    const usersWinCount = await this.prisma.tbl_lotto_purchase_orders.groupBy({
+      by: ['user_id'],
+      where: {
+        variant_id,
+        win_rank: {
+          not: 0,
+        },
+      },
+      _count: {
+        user_id: true,
+      },
+      orderBy: {
+        user_id: 'asc',
+      },
+    });
+
+    const winCountMap = usersWinCount.reduce((acc, curr) => {
+      acc[curr.user_id] = curr._count.user_id;
+      return acc;
+    }, {});
+
+    const result = usersOrderCount.map((user) => {
+      const winCount = winCountMap[user.user_id] || 0;
+      return {
+        user_id: user.user_id,
+        passes: user._count.user_id,
+        win_count: winCount,
+      };
+    });
+
+    const totalWinCount = result.reduce((acc, curr) => acc + curr.win_count, 0);
+
+    // Counting total records for pagination
+    const totalRecords = await this.prisma.tbl_lotto_purchase_orders.count({
+      where: { variant_id },
+    });
+
+    return {
+      users: result,
+      total_win_count: totalWinCount,
+      total_records: totalRecords,
+      current_page: page,
+      total_pages: Math.ceil(totalRecords / limit),
+    };
+  }
+
+  async findPassNumbersByUserAndVariant(
+    user_id: number,
+    variant_id: number,
+    page: number,
+    limit: number,
+  ) {
+    const skip = (page - 1) * limit;
+    const take = limit;
+
+    const [data, total] = await this.prisma.$transaction([
+      this.prisma.tbl_lotto_purchase_orders.findMany({
+        where: {
+          user_id,
+          variant_id,
+        },
+        select: {
+          pass_number: true,
+          created_at: true,
+        },
+        skip,
+        take,
+        orderBy: {
+          user_id: 'asc',
+        },
+      }),
+      this.prisma.tbl_lotto_purchase_orders.count({
+        where: {
+          user_id,
+          variant_id,
+        },
+      }),
+    ]);
+
+    return {
+      data,
+      total,
+      page,
+      pageCount: Math.ceil(total / limit),
+    };
+  }
+
+  async findOrdersByVariantAndWinRank(variant_id: number, win_rank: number) {
+    return this.prisma.tbl_lotto_purchase_orders.findMany({
+      where: {
+        variant_id,
+        win_rank,
+      },
+      include: {
+        product: true,
+        variant: true,
+      },
+    });
+  }
 }
